705e369039c65bd1c4c5981ae95275b9f41ed8cc
Zeeeen0 <hoo1042@gmail.com>
2024-12-14 19:59:53 +0900

    init: Initialize Spring Boot Kotlin project with Gradle

---
+++ src/main/kotlin/com/toss/learnershigh/LearnersHighApplication.kt
@@ -0,0 +1,11 @@
+package com.toss.learnershigh
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnersHighApplication
+
+fun main(args: Array<String>) {
+    runApplication<LearnersHighApplication>(*args)
+}
---
+++ src/test/kotlin/com/toss/learnershigh/LearnersHighApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toss.learnershigh
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearnersHighApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
